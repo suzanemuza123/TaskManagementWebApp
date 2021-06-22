37e2e8a97f6a2b293bf523bb5b05310d2b9a1d97
suzanemuza123 <72234636+suzanemuza123@users.noreply.github.com>
2021-06-21 22:56:59 -0500

    Create actionsdeleteContactInfo.js

---
+++ actionsdeleteContactInfo.js
@@ -0,0 +1,10 @@
+import { DELETE_CONTRACT_INFO } from "../constants/constants.js";
+
+const deleteContractInfo = id => {
+  return {
+    type: DELETE_PROFILE_INFO
+    id: id
+  };
+};
+
+export default deleteProfileInfo;
