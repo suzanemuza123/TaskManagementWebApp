4179092edd930077bb6deec4d92afab85928552e
suzanemuza123 <72234636+suzanemuza123@users.noreply.github.com>
2021-06-23 21:19:15 -0500

    Create deletebutton.jsx

---
+++ deletebutton.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const showAll = props => {
+  var style = {
+    width: 80,
+    height: 80
+  };
+  return (
+    <div>
+      <button
+        disabled={
+          props.createNewList === null ||
+          props.createTask[props.createNewList] === false
+        }
+        onClick={() =
+        props .onAdd(props.id)
+        }
+        style={style}
+      >
+        {props.id}
+      </button>
+    </div>
+  );
+};
+
+export default showAll;
