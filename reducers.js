99a4cc90a18d6cdb1e66d38df36ef3e3d2e947e4
suzanemuza123 <72234636+suzanemuza123@users.noreply.github.com>
2021-06-26 09:42:08 -0500

    Update reducers.js

--- reducers.js
+++ reducers.js
@@ -21,17 +21,78 @@ var initialtodolistStatusData = new Array(16).fill(false);
 
 const setupTask = (state = initialtodolist, action) => {
   switch (action.type) {
-    case CREATE_CONTACT:
+    case CREATE_NEW_LIST:
       var stateCopy = [];
       for (let i = 0; i < 16; i++) {
         stateCopy.push(state[1].slice());
       }
       stateCopy[action.id].push(action.id);
       return stateCopy;
-    case CREATE_NEW_LIST:
+    case MARK_AS_COMPLETE:
       var stateCopy = [];
       for (let i = 0; i < 16; i++) {
         stateCopy.push(state[i].slice());
       }
+      console.log(action);
+      stateCopy[action.id].splice(action.id);
+      return stateCopy;
+    case SUBMIT:
+      var stateCopy = [];
+      for (let i = 0; i < 16; i++) {
+        stateCopy.push(state[i].slice());
+      }
+      console.log(action);
+      stateCopy[action.id].splice(action.id);
+      return stateCopy;
+    case SHOW_ACTIVE:
+      var stateCopy = [];
+      for (let i = 0; i < 16; i++) {
+        stateCopy.push(state[i].slice());
+      }
+      console.log(action);
+      stateCopy[action.id].splice(action.id);
+      return stateCopy;
+    case DELETE_FROM_LIST:
+      var stateCopy = [];
+      for (let i = 0; i < 16; i++) {
+        stateCopy.push(state[i].slice());
+      }
+      console.log(action);
+      stateCopy[action.id].splice(action.id);
+      return stateCopy;
+    case SHOW_ALL:
+      var stateCopy = [];
+      for (let i = 0; i < 16; i++) {
+        stateCopy.push(state[i].slice());
+      }
+      console.log(action);
+      stateCopy[action.id].splice(action.id);
+      return stateCopy;
+    case SHOW_COMPLETED:
+      var stateCopy = [];
+      for (let i = 0; i < 16; i++) {
+        stateCopy.push(state[i].slice());
+      }
+      stateCopy[action.id] = [];
+      return stateCopy;
+    default:
+      return state;
   }
-}
+};
+
+const CreateAContact = (state = 0, action) => {
+  switch (action.type) {
+    case CREATE_CONTACT:
+      if (state === action.id) return null;
+      else return action.id;
+    default:
+      return state;
+  }
+};
+
+const reducer = combineReducers({
+  CreateAContact,
+  setupTask
+});
+
+export default reducer;
